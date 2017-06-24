35058aa5596cab4af9d5b20023648d983f495ac4
Bryan Tabarez <bryan.tabarez@correounivalle.edu.co>
2017-06-24 16:48:51 +0000

    app-routing, app.component, app.module Added routing, update main component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule }    from '@angular/http';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap'; // third-party
 
 import { AppComponent } from './app.component';
-import { TmdbApiService } from './tmdb-api.service';
+import { AppRoutingModule }     from './app-routing.module';
+import { TmdbService } from './tmdb.service';
+
+import { MoviesComponent } from './movies/movies.component';
+import { MovieDetailComponent } from './movie-detail/movie-detail.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesComponent,
+    MovieDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    NgbModule.forRoot(),
+    AppRoutingModule
   ],
-  providers: [TmdbApiService],
+  providers: [TmdbService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
