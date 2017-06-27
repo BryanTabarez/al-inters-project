1e87d23ee3414b9cf0917a617778b87378be45fe
BryanTabarez <userbryan@gmail.com>
2017-06-27 07:32:57 -0500

    Preview of a person's information and modal with additional details

--- src/app/person-card/person-card.component.ts
+++ src/app/person-card/person-card.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Person } from '../person';
 import { API_IMG } from '../movie.constants';
 
+import { TmdbService } from '../tmdb.service';
+
 @Component({
   selector: 'app-person-card',
   templateUrl: './person-card.component.html',
@@ -10,10 +12,19 @@ import { API_IMG } from '../movie.constants';
 export class PersonCardComponent implements OnInit {
   @Input() person: Person;
   apiImg = API_IMG;
+  infoPerson: Person;
 
-  constructor() { }
+  constructor(
+    private apiService: TmdbService,
+  ) { }
 
   ngOnInit() {
+  }
+
+  getInfoPerson(person_id: number) {
+    this.apiService.getPersonDetails(person_id).then(
+      data => this.infoPerson = data
+    )
   }
 
 }
